9e4b3007e0b631d6d472a440487c82c0e7c3a0bf
Tim Metzler <tim.metzler@h-brs.de>
2020-08-28 15:10:52 +0200

    Add RemoveExercise preprocessor

---
+++ nbassignment/preprocessors/removeexercise.py
@@ -0,0 +1,23 @@
+import os
+import shutil
+from .preprocessor import Preprocessor
+
+class RemoveExercise(Preprocessor):
+
+    def preprocess(self, resources):
+        base_path = os.path.join(
+            'source',
+            resources['assignment']
+        )
+        exercise_files = os.path.join(
+            base_path,
+            '{}_files'.format(resources['exercise'])
+        )
+        if os.path.exists(exercise_files):
+            shutil.rmtree(exercise_files)
+        exercise_nb = os.path.join(
+            base_path,
+            '{}.ipynb'.format(resources['exercise'])
+        )
+        if os.path.exists(exercise_nb):
+            os.remove(exercise_nb)

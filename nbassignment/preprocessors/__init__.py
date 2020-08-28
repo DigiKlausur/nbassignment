ea6a21dcbefdf606711d5fa115caec9cb6e30b3a
Tim Metzler <tim.metzler@h-brs.de>
2020-08-28 15:13:06 +0200

    Register preprocessors

--- nbassignment/preprocessors/__init__.py
+++ nbassignment/preprocessors/__init__.py
@@ -1,1 +1,7 @@
 from .preprocessor import Preprocessor
+from .removeexercise import RemoveExercise
+from .copynotebooks import CopyNotebooks
+from .copyfiles import CopyFiles
+from .generatetaskids import GenerateTaskIDs
+from .makeexercise import MakeExercise
+from .filltemplate import FillTemplate

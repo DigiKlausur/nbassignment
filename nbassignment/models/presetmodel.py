b789b7449b2675ac4b5e81a644880005c8e206d7
Tim Metzler <tim.metzler@h-brs.de>
2020-09-08 13:28:31 +0200

    Refactor preset model

--- nbassignment/models/presetmodel.py
+++ nbassignment/models/presetmodel.py
@@ -13,17 +13,38 @@ class PresetModel(LoggingConfigurable):
             'nbassignment/server_extensions/taskcreator/presets/questions/'
         )
     ).tag(config=True)
-    
-    def list_question_presets(self):
+
+    template_preset_path = Unicode(
+        os.path.join(
+            os.path.dirname(__file__),
+            '..',
+            '..',
+            'nbassignment/server_extensions/taskcreator/presets/template/'
+        )
+    ).tag(config=True)
+
+    def list_presets(self, preset_path):
         presets = []
-        for item in os.listdir(self.task_preset_path):
-            if os.path.isfile(os.path.join(self.task_preset_path, item)) and \
+        for item in os.listdir(preset_path):
+            if os.path.isfile(os.path.join(preset_path, item)) and \
                item.endswith('.ipynb'):
                 presets.append(os.path.splitext(item)[0])
         return presets
-    
-    def get_question_preset(self, preset_name):
-        path = os.path.join(self.task_preset_path, '{}.ipynb'.format(preset_name))
+
+    def get_preset(self, preset_path, preset_name):
+        path = os.path.join(preset_path, '{}.ipynb'.format(preset_name))
         if os.path.isfile(path):
             nb = nbformat.read(path, as_version=4)
-            return nb.cells    
+            return nb.cells 
+
+    def list_question_presets(self):
+        return self.list_presets(self.task_preset_path)
+
+    def get_question_preset(self, preset_name):
+        return self.get_preset(self.task_preset_path, preset_name)
+
+    def list_template_presets(self):
+        return self.list_presets(self.template_preset_path)
+
+    def get_template_preset(self, preset_name):
+        return self.get_preset(self.template_preset_path, preset_name)

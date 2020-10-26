94d81791726655cba681db16e69721f654c09827
Tim Metzler <tim.metzler@h-brs.de>
2020-10-26 14:58:18 +0100

    Add course directory

--- nbassignment/models/taskpoolmodel.py
+++ nbassignment/models/taskpoolmodel.py
@@ -2,22 +2,29 @@ import os
 import nbformat
 import shutil
 from .basemodel import BaseModel
+from traitlets import Unicode
 
 class TaskPoolModel(BaseModel):
 
+    directory = Unicode(
+        'pools',
+        help='The directory where the task pools go.'
+    )
+
     def new(self, name):
-        path = os.path.join('pools', name)
+        path = os.path.join(self.base_path(), name)
         os.makedirs(path, exist_ok=True)
         url = os.path.join('/', 'taskcreator', 'pools', name)
         return url
 
     def remove(self, name):
-        shutil.rmtree(os.path.join('pools', name))
+        path = os.path.join(self.base_path(), name)
+        shutil.rmtree(path)
     
     def list(self):
-        if not os.path.isdir('pools'):
-            os.makedirs('pools', exist_ok=True)
-        poolfolders = os.listdir('pools')
+        if not os.path.isdir(self.base_path()):
+            os.makedirs(self.base_path(), exist_ok=True)
+        poolfolders = os.listdir(self.base_path())
         pools = []
         for poolfolder in poolfolders:
             tasks = self.__get_pool_info(poolfolder)
@@ -30,4 +37,4 @@ class TaskPoolModel(BaseModel):
         return pools
 
     def __get_pool_info(self, name):
-        return len(os.listdir(os.path.join('pools', name)))
+        return len(os.listdir(os.path.join(self.base_path(), name)))
